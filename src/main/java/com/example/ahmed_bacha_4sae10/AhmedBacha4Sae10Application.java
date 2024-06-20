bccdeed70ac397c73ad9b38adb9a235688174956
Mariem-Chebbi <mariem.chebbi@esprit.tn>
2024-06-20 22:52:52 +0100

    test

---
+++ src/main/java/com/example/ahmed_bacha_4sae10/AhmedBacha4Sae10Application.java
@@ -0,0 +1,13 @@
+package com.example.ahmed_bacha_4sae10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmedBacha4Sae10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhmedBacha4Sae10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ahmed_bacha_4sae10/AhmedBacha4Sae10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ahmed_bacha_4sae10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmedBacha4Sae10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
